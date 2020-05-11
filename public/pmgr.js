97da4fcceeb5ef83e3bc15ccedc335ed62ca377b
Rusty Shackleford <gigolo@hotmail.co.uk>
2020-05-11 06:36:52 +0100

    using 98.css instead, refactoring

--- public/pmgr.js
+++ public/pmgr.js
@@ -29,7 +29,8 @@ let find = (sel, pel = document) => {
     p = 'getElementById';
     sel = sel.slice(1);
   }
-  return pel[p](sel);
+  let ret = pel[p](sel);
+  return ret.length === undefined ? ret : (ret.length === 1 ? ret[0] : ret);
 };
 
 let on = (el, action, cb) => {
@@ -40,11 +41,33 @@ let del = (el) => {
   el.parentNode.removeChild(el);
 };
 
+let dom = (t, c = false, v = false) => `<${t}${v ? ' ' + Object.entries(v).map(a => a[0] + '="' + a[1] + '"').join(' ') : ''}${c ? '>' + c + '</' + t + '>' : '/>'}`;
+
 let log = console.log;
 
-let new_draggable = (el) => {
+let create_window = (id, w=0, h=0) => {
+  let d = dom('div',
+    dom('div',
+      dom('div', 'test [test]', {'class': 'title-bar-text'}) +
+      dom('div', dom('button', '', {'aria-label': 'Close'}), {'class': 'title-bar-controls'}),
+    {'class': 'title-bar', 'id': id}) +
+    dom('div', '', {'class': 'window-body'}) +
+    dom('div', '', {'class': 'window-resize', 'id': id}),
+  {'class': 'window window-center', 'id': id});
+  document.body.insertAdjacentHTML('beforeend', d);
+  let el = find('.window#' + id);
+  on(find('.title-bar-controls button', el), 'click', (e) => {
+    del(el);
+  });
+  if (w !== 0 && h !== 0) {
+    el.style.width  = w + "px";
+    el.style.height = h + "px";
+  }
+  return el;
+};
+
+let draggable = (el) => {
   let x = 0, y = 0, dx = 0, dy = 0;
-  let el_id = el.dataset['id'];
   
   let init = (e, move_fn) => {
     e = e || window.event;
@@ -55,25 +78,21 @@ let new_draggable = (el) => {
     document.onmousemove = move_fn;
   };
   
-  on(find(`#${el_id}_header`), 'mousedown', e => init(e, start_drag));
-  on(find(`#${el_id}_resize`), 'mousedown', e => init(e, start_resize));
-  on(find(`#${el_id}_del`), 'click', (e) => {}); // TODO: Delete entry from redis/fs
-  on(find(`#${el_id}_close`), 'click', (e) => {
-    del(el);
-    find(`#${el_id}_menu`).classList.toggle('opened');
-  });
+  el.classList.remove('window-center');
+  on(find('.title-bar', el), 'mousedown', e => init(e, start_drag));
+  on(find('.window-resize', el), 'mousedown', e => init(e, start_resize));
   
   let update = (e) => {
     e = e || window.event;
     e.preventDefault();
-    dx = x - e.clientX;
-    dy = y - e.clientY;
+    dx = e.clientX - x;
+    dy = e.clientY - y;
     x  = e.clientX;
     y  = e.clientY;
   };
   
   let start_drag = (e) => {
-    update();
+    update(e);
     
     let dbbcr = document.body.getBoundingClientRect();
     let dbw = dbbcr.width;
@@ -83,19 +102,19 @@ let new_draggable = (el) => {
     let elh = elbcr.height;
     let maxx = dbw - elw - 1;
     let maxy = dbh - elh - 1;
-    let ww = clamp(el.offsetLeft - dx, 1, maxx);
-    let wh = clamp(el.offsetTop  - dy, 1, maxy);
+    let ww = clamp(el.offsetLeft + dx, 1, maxx);
+    let wh = clamp(el.offsetTop  + dy, 1, maxy);
     
     el.style.left = ww + "px";
     el.style.top  = wh + "px";
   };
   
   let start_resize = (e) => {
-    update();
+    update(e);
     
-    let bcr = el.getBoundingClientRect();
-    let nw = Math.max(bcr.width - dx, 200);
-    let nh = Math.max(bcr.height - dy, 200);
+    let cs = getComputedStyle(el, '');
+    let nw = Math.max(parseInt(cs.width) + dx, 200);
+    let nh = Math.max(parseInt(cs.height) + dy, 200);
     
     el.style.width  = nw + "px";
     el.style.height = nh + "px";
@@ -107,160 +126,20 @@ let new_draggable = (el) => {
   };
 };
 
-let new_window = (id, type) => {
-  if (type === 'projects') {
-    // TODO: Project window handling
-  } else {
-    let window = document.createElement('div');
-    window.classList = ['window'];
-    window.id = id + '_window';
-    window.dataset['id'] = id
-    let window_cont = document.createElement('div');
-    window_cont.classList = ['window_cont'];
-    let window_head = document.createElement('div');
-    window_head.classList = ['window_head'];
-    window_head.id = id + '_header';
-    let window_title = document.createElement('span');
-    window_title.classList = ['window_title'];
-    window_title.innerHTML = 'test <b>[test]</b>';
-    let window_controls = document.createElement('div');
-    window_controls.classList = ['window_controls'];
-    let window_edit = document.createElement('div');
-    window_edit.id = id + '_edit';
-    window_edit.classList = ['window_ctrl'];
-    window_edit.innerText = 'edit';
-    let window_del = document.createElement('div');
-    window_del.id = id + '_del';
-    window_del.classList = ['window_ctrl'];
-    window_del.innerText = 'del';
-    let window_close = document.createElement('div');
-    window_close.id = id + '_close';
-    window_close.classList = ['window_close button'];
-    window_close.innerText = 'X';
-    let window_body_cont = document.createElement('div');
-    window_body_cont.classList = ['window_body_cont'];
-    let window_body = document.createElement('div');
-    window_body.classList = ['window_body'];
-    let window_resize = document.createElement('div');
-    window_resize.classList = ['window_resize'];
-    window_resize.id = id + '_resize';
-  
-    window_head.appendChild(window_title);
-    window_controls.appendChild(window_edit);
-    window_controls.appendChild(window_del);
-    window_controls.appendChild(window_close);
-    window_head.appendChild(window_controls);
-    window_cont.appendChild(window_head);
-    window_body_cont.appendChild(window_body);
-    window_cont.appendChild(window_body_cont);
-    window.appendChild(window_cont);
-    window.appendChild(window_resize);
-  
-    // TODO: Store width, height, x, y, opened
-    let ww = 640, wh = 480;
-    let dbbcr = document.body.getBoundingClientRect();
-    let wx = dbbcr.width / 2  - ww / 2;
-    let wy = dbbcr.height / 2 - wh / 2;
-  
-    window.style.left   = wx + "px";
-    window.style.top    = wy + "px";
-    window.style.width  = ww + "px";
-    window.style.height = wh + "px";
-  
-    document.body.appendChild(window);
-    
-    get('/api/get/' + type + '/' + id, (json) => {
-      get('/data/' + id + '.md', (data) => {
-        console.log(data);
-      });
-    });
-  
-    return window;
-  }
-};
-
-let menu_object_cb = (e) => {
+let menu_cb = (e) => {
   let type = e.target.dataset['type'];
-  let id = e.target.dataset['id'];
-  if (type === 'projects') {
-    // TODO: Handle project window opening/closing
-    let li = find('#projects_menu').getElementsByTagName('li');
-    for (let i = 0; i < li.length; i++) {
-      if (li[i].firstElementChild === e.target)
-        continue;
-      li[i].firstElementChild.classList.remove('opened');
-    }
+  if (type === 'projects')
+    return; // TODO
+  if (e.target.classList.contains('opened')) {
+    e.target.classList.remove('opened');
+    del(find('.window#' + e.target.id));
   } else {
-    if (e.target.classList.contains('opened')) {
-      e.target.classList.remove('opened');
-      del(find(`#${id}_window`));
-    } else {
-      e.target.classList.add('opened');
-      new_draggable(new_window(id, type)); // TODO: Merge new_draggable with new_window
-    }
+    e.target.classList.add('opened');
+    draggable(create_window(e.target.id));
   }
 };
 
 on(document, 'DOMContentLoaded', (e) => {
-  for (var o of find('.caret'))
-    on(o, 'click', (e) => {
-      e.target.parentElement.querySelector('.nested').classList.toggle('inactive');
-      e.target.classList.toggle('caret-down');
-    });
-  
-  let menu = find('#menu');
-  on(find('#show_menu'), 'click', (e) => {
-    menu.style.visibility = 'visible';
-  });
-  
-  on(find('#hide_menu'), 'click', (e) => {
-    menu.style.visibility = 'hidden';
-  });
-  
-  let add = find('.add');
-  let add_box = find('#add');
-  let add_title = find('#add_title');
-  let search = find('#search');
-  let search_type = find('#search_type');
-  
-  for (let o of add)
-    on(o, 'click', (e) => {
-      let type = e.target.dataset['type'];
-      add_title.innerText = "Add to " + capitalise(type);
-      search_type.value = type;
-      if (add_box.classList.contains('inactive'))
-        add_box.classList.remove('inactive');
-    });
-
-  on(find('#add_close'), 'click', (e) => {
-    add_box.classList.toggle('inactive');
-  });
-  on(search, 'keydown', (e) => {
-    if (e.keyCode != 13)
-      return;
-
-    let type = search_type.value;
-    post('/api/add',
-    JSON.stringify({
-      type: type,
-      title: search.value
-    }), (json) => {
-      data = JSON.parse(json);
-      let li = document.createElement('li');
-      let span = document.createElement('span');
-      span.classList = ['object'];
-      span.id = data['id'] + '_menu';
-      span.dataset['type'] = data['type'];
-      span.dataset['id'] = data['id'];
-      span.innerText = data['title'];
-      on(span, 'click', menu_object_cb);
-      li.appendChild(span);
-      find(`#${data['type']}_menu`).appendChild(li);
-      add_box.classList.toggle('inactive');
-    });
-    search.value = '';
-  });
-  
-  for (var o of find('.object'))
-    on(o, 'click', menu_object_cb);
+  for (let li of find('.tree-view-li'))
+    on(li, 'click', menu_cb);
 });
